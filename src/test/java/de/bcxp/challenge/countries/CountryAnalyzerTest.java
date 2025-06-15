e3e2b2d5ecbee0303e45ccda7134fce1242598f7
rachel <selina_barbara.rachel@student.reutlingen-university.de>
2025-06-15 20:35:41 +0200

    test: add tests for CountryAnalyzer

---
+++ src/test/java/de/bcxp/challenge/countries/CountryAnalyzerTest.java
@@ -0,0 +1,36 @@
+package de.bcxp.challenge.countries;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+public class CountryAnalyzerTest {
+
+    @Test
+    void shouldGetRightCountryWithHighestPopulationDensity() {
+        List<CountryData> countryDataList = List.of(
+                new CountryData("Austria", 8926000, 83855), // spread = 10
+                new CountryData("Malta", 516100, 316), // spread = 3
+                new CountryData("Belgium", 11566041, 30528) // spread = 5
+        );
+        CountryAnalyzer analyzer = new CountryAnalyzer();
+        String result = analyzer.getMaxPopulationDensity(countryDataList);
+        assertEquals("Malta", result);
+    }
+
+    @Test
+    void shouldThrowIllegalArgumentException() {
+        List<CountryData> countryDataList = List.of(); // Empty list
+        CountryAnalyzer analyzer = new CountryAnalyzer();
+        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> {
+            analyzer.getMaxPopulationDensity(countryDataList);
+        });
+        assertTrue(exception.getMessage().contains("No valid country data found"));
+
+    }
+
+}
